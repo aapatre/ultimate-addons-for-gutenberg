75facc92c7f9533b916c4f739db4ef1dd3ce3c00
RajkiranBagal <rajkiranb@bsf.io>
2022-02-01 18:01:03 +0530

    lint

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'a05aaafa8f4ad198a27e4ddfe0fac511');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '201d6412eaf6278ba40348b1dd91aa40');

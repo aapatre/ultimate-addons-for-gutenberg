ee4ae8168113985cee576779c770b7d58a388964
RajkiranBagal <rajkiranb@bsf.io>
2022-02-04 16:44:34 +0530

    Updated multiple blocks SVG's

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '1240023eedb014cac93f6e73e3fde105');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '7874317e2aa0e924a89c1498e81bbe32');

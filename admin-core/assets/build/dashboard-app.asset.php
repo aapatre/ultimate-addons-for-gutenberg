433de34a1ae4d8d1ec497b71f40faefb6a9dfc4c
RajkiranBagal <rajkiranb@bsf.io>
2022-02-09 11:34:59 +0530

    fix: Added Dynamic RSS Feed for UAG Changelog in Admin dashbaord

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'e23c86043034d2fe5e0559958af0d686');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '117b2c317cd36730d929e81f6ee866b9');

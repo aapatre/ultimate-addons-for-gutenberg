5795dfaf4e9c668fbb7a7192197c92147be9a813
RajkiranBagal <rajkiranb@bsf.io>
2022-02-10 18:35:00 +0530

    fix: Admin Styling changes suggested by designers.

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '5bf7d43015e369c76914907d31f57c32');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '37b1adb23789b75c4c3ff0b25dc3511c');

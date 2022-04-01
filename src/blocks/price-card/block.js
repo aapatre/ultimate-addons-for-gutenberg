ac14c7e4f62cd8c3e867145bc5ac48e201d0894a
Antariksh Patre <antarikshpatre@gmail.com>
2022-04-01 11:55:14 +0530

    Added comments to block.js for test price card block

--- src/blocks/price-card/block.js
+++ src/blocks/price-card/block.js
@@ -11,16 +11,16 @@ import save from './save';
 
 registerBlockType( 'uagb/price-card', {
     title: __( 'Price Card', 'ultimate-addons-for-gutenberg' ), // Block title.
-    description: __( 'A block that allows you to add a cool price card', 'ultimate-addons-for-gutenberg' ),
-    icon: UAGB_Block_Icons.restaurant_menu,
-    keywords: [ __( 'price' ), __( 'card' ), __( 'uag' ) ],
-    category: uagb_blocks_info.category,
-    attributes: {},
+    description: __( 'A block that allows you to add a cool price card', 'ultimate-addons-for-gutenberg' ), // Block description.
+    icon: UAGB_Block_Icons.restaurant_menu, // Block icon.
+    keywords: [ __( 'price' ), __( 'card' ), __( 'uag' ) ], // Keywords for the block.
+    category: uagb_blocks_info.category,    // Block category.
+    attributes: {}, // Block attributes.
     edit,
     save,
     example: {
         attributes: {
             isPreview: true,
         }
-    },
+    },  // An empty object to render the preview.
 } );

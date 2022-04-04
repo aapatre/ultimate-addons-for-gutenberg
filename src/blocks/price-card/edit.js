fa7a88a3bf8606d8e30355353e145e6190ad271b
Antariksh Patre <antarikshpatre@gmail.com>
2022-04-04 10:57:05 +0530

    Created save, render, settings, styling JS files and setup basic edit.js imports

---
+++ src/blocks/price-card/edit.js
@@ -0,0 +1,16 @@
+/**
+ * BLOCK: Price Card - Edit Class
+ */
+
+import styling from './styling';
+import { select, dispatch } from '@wordpress/data';
+import React, { lazy, Suspense, useEffect } from 'react';
+import lazyLoader from '@Controls/lazy-loader';
+import { useDeviceType } from '@Controls/getPreviewType';
+import addBlockEditorDynamicStyles from '@Controls/addBlockEditorDynamicStyles';
+const Settings = lazy( () =>
+	import( /* webpackChunkName: "chunks/price-list/settings" */ './settings' )
+);
+const Render = lazy( () =>
+	import( /* webpackChunkName: "chunks/price-list/render" */ './render' )
+);

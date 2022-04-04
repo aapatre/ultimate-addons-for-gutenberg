ddbc6e52603416d93162042e82bbbddd99c0dbbc
Antariksh Patre <antarikshpatre@gmail.com>
2022-04-04 11:59:25 +0530

    Added UseLayoutEffect in render.js

--- src/blocks/price-card/render.js
+++ src/blocks/price-card/render.js
@@ -3,3 +3,14 @@ import React, { useLayoutEffect, useMemo } from 'react';
 import { InnerBlocks } from '@wordpress/block-editor';
 import styles from './editor.lazy.scss';
 import { useDeviceType } from '@Controls/getPreviewType';
+
+const Render = ( props ) => {
+    // Add and remove the CSS on the drop and remove of the component.
+	useLayoutEffect( () => {
+		styles.use();
+		return () => {
+			styles.unuse();
+		};
+	}, [] );
+}
+export default React.memo( Render );

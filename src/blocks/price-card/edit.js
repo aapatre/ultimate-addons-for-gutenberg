ddd5fc91b833badb7716945973d6413d2761c01e
Antariksh Patre <antarikshpatre@gmail.com>
2022-04-05 13:16:48 +0530

    Fixed issue with useEffect hook in edit.js

--- src/blocks/price-card/edit.js
+++ src/blocks/price-card/edit.js
@@ -15,3 +15,43 @@ const Render = lazy( () =>
 	import( /* webpackChunkName: "chunks/price-list/render" */ './render' )
 );
 
+const UAGBPriceCardEdit = ( props ) => {
+
+	const deviceType = useDeviceType();
+
+    useEffect( () => {
+
+    	const { className, clientId, attributes, setAttributes } = props;
+
+        // Assigning block_id in the attribute.
+		setAttributes( { block_id: clientId.substr( 0, 8 ) } );
+
+        const {
+            classMigrate,
+            block_id,
+            title,
+            description,
+            image,
+            priceSmall,
+            priceMedium,
+            priceLarge,
+            smallVariantText,
+            mediumVariantText,
+            largeVariantText,
+            smallVariantExists,
+            mediumVariantExists,
+            largeVariantExists
+        } = attributes;
+
+    }, [] );
+
+    return(
+        <Suspense fallback={ lazyLoader() }>
+            <Settings parentProps={ props } />
+            <Render parentProps={ props } /> 
+        </Suspense>
+    );
+
+};
+
+export default UAGBPriceCardEdit;

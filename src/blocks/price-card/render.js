a65f5839e6e0297eac69e33461aeb187d5735c3e
Antariksh Patre <antarikshpatre@gmail.com>
2022-04-07 10:54:27 +0530

    Created rendering logic and DOM structure for the case where only one product variant is present

--- src/blocks/price-card/render.js
+++ src/blocks/price-card/render.js
@@ -1,5 +1,5 @@
 import classnames from 'classnames';
-import React, { useLayoutEffect, useMemo } from 'react';
+import React, { useLayoutEffect, useMemo, useEffect, useState } from 'react';
 import { InnerBlocks, RichText } from '@wordpress/block-editor';
 import { __ } from '@wordpress/i18n';
 import styles from './editor.lazy.scss';
@@ -17,6 +17,7 @@ const Render = ( props ) => {
     props = props.parentProps;
 	const deviceType = useDeviceType();
 	const { className, attributes, setAttributes } = props;
+    let [countOfVariants, setCountOfVariants] = useState( 0 );
 
     const {
         classMigrate,
@@ -34,6 +35,14 @@ const Render = ( props ) => {
         mediumVariantExists,
         largeVariantExists
     } = attributes;
+
+    // This hook will check the number of product variants that are currently active (updates in realtime).
+    useEffect( () => {
+        
+        // Get how many variants are currently active.
+        setCountOfVariants( [smallVariantExists, mediumVariantExists, largeVariantExists].filter( Boolean ).length );
+
+    }, [smallVariantExists, mediumVariantExists, largeVariantExists] );
 
     return(
         <div className={ classnames(
@@ -58,6 +67,35 @@ const Render = ( props ) => {
                 value={ description }
                 onChange={ ( content ) => setAttributes( { description: content } ) }
             />
+
+            {/* In case only one variant render either of these instead of a dropdown selector */}
+
+            { countOfVariants === 1 && smallVariantExists &&
+                <>
+                    <h4>
+                        <span>{smallVariantText}:</span>
+                        <strong><span> {priceSmall}</span></strong>
+                    </h4>
+                </>
+            }
+
+            { countOfVariants === 1 && mediumVariantExists &&
+                <>
+                    <h4>
+                        <span>{mediumVariantText}:</span>
+                        <strong><span> {priceMedium}</span></strong>
+                    </h4>
+                </>
+            }
+
+            { countOfVariants === 1 && largeVariantExists &&
+                <>
+                    <h4>
+                        <span>{largeVariantText}:</span>
+                        <strong><span> {priceLarge}</span></strong>
+                    </h4>
+                </>
+            }
 
         </div>
     );

3f7ebe52e70d44dcb926ef78b3d9867ca7efb49e
Antariksh Patre <antarikshpatre@gmail.com>
2022-04-07 14:42:50 +0530

    Added functional logic and DOM for rendering selected variant in post editor

--- src/blocks/price-card/render.js
+++ src/blocks/price-card/render.js
@@ -4,6 +4,7 @@ import { InnerBlocks, RichText } from '@wordpress/block-editor';
 import { __ } from '@wordpress/i18n';
 import styles from './editor.lazy.scss';
 import { useDeviceType } from '@Controls/getPreviewType';
+import PriceDisplay from './components/PriceDisplay';
 
 const Render = ( props ) => {
     // Add and remove the CSS on the drop and remove of the component.
@@ -18,6 +19,7 @@ const Render = ( props ) => {
 	const deviceType = useDeviceType();
 	const { className, clientId, attributes, setAttributes } = props;
     let [countOfVariants, setCountOfVariants] = useState( 0 );
+    let [currentVariantId, setCurrentVariantId] = useState( null );  // Currently displayed variant (0 = small, 1 = medium, 2 = large).
 
     const {
         classMigrate,
@@ -71,30 +73,15 @@ const Render = ( props ) => {
             {/* In case only one variant render either of these instead of a dropdown selector */}
 
             { countOfVariants === 1 && smallVariantExists &&
-                <>
-                    <h4>
-                        <span>{smallVariantText}:</span>
-                        <strong><span> {priceSmall}</span></strong>
-                    </h4>
-                </>
+                <PriceDisplay variantName={smallVariantText} price={priceSmall} />
             }
 
             { countOfVariants === 1 && mediumVariantExists &&
-                <>
-                    <h4>
-                        <span>{mediumVariantText}:</span>
-                        <strong><span> {priceMedium}</span></strong>
-                    </h4>
-                </>
+                <PriceDisplay variantName={mediumVariantText} price={priceMedium} />
             }
 
             { countOfVariants === 1 && largeVariantExists &&
-                <>
-                    <h4>
-                        <span>{largeVariantText}:</span>
-                        <strong><span> {priceLarge}</span></strong>
-                    </h4>
-                </>
+                <PriceDisplay variantName={largeVariantText} price={priceLarge} />
             }
 
             {/* If more variants exist, render a dropdown selector to see the price of the product. */}
@@ -103,18 +90,40 @@ const Render = ( props ) => {
                     <label htmlFor={ 'uagb-price-card-variant-selector-' + clientId.substr( 0, 8 ) } >
                         { __( 'Choose variant: ', 'ultimate-addons-for-gutenberg' ) }
                     </label>
-                    <select name={ 'uagb-price-card-variant-selector-' + clientId.substr( 0, 8 ) } >
+                    {/* eslint-disable jsx-a11y/no-onchange */}
+                    <select
+                        onChange={ ( e ) => setCurrentVariantId( e.target.value )  }
+                        name={ 'uagb-price-card-variant-selector-' + clientId.substr( 0, 8 ) }
+                    >
+                        {/* eslint-enable jsx-a11y/no-onchange */}
                         <option selected disabled hidden>{ __( 'Select variant', 'ultimate-addons-for-gutenberg' ) }</option>
+                        
                         { smallVariantExists &&
-                            <option value={smallVariantText}>{smallVariantText}</option>
+                            <option value={0}>{smallVariantText}</option>
                         }
                         { mediumVariantExists &&
-                            <option value={mediumVariantText}>{mediumVariantText}</option>
+                            <option value={1}>{mediumVariantText}</option>
                         }
                         { largeVariantExists &&
-                            <option value={largeVariantText}>{largeVariantText}</option>
+                            <option value={2}>{largeVariantText}</option>
                         }
+
                     </select>
+
+                    {/* Render the price of the selected variant. */}
+
+                    { currentVariantId === '0' &&
+                        <PriceDisplay variantName={smallVariantText} price={priceSmall} />
+                    }
+
+                    { currentVariantId === '1' &&
+                        <PriceDisplay variantName={mediumVariantText} price={priceMedium} />
+                    }
+
+                    { currentVariantId === '2' &&
+                        <PriceDisplay variantName={largeVariantText} price={priceLarge} />
+                    }
+
                 </>
             }
 
